dd290ad45139bc03186c24a21df3a0f3b24ca6a0
Ugurismail <uguraygun001@hotmail.com>
2022-11-23 15:42:37 +0300

    colors full ok

--- djdict/settings_prod.py
+++ djdict/settings_prod.py
@@ -70,14 +70,14 @@ TEMPLATES = [
 WSGI_APPLICATION = "djdict.wsgi.application"
 
 DATABASES = {
-    "default": {
-        "ENGINE": os.environ.get("SQL_ENGINE"),
-        "NAME": os.environ.get("SQL_DATABASE"),
-        "USER": os.environ.get("SQL_USER", "user"),
-        "PASSWORD": os.environ.get("SQL_PASSWORD"),
-        "HOST": os.environ.get("SQL_HOST"),
-        "PORT": os.environ.get("SQL_PORT"),
-    }
+'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'postgres',
+    'USER': 'db_dictionary_user',
+    'PASSWORD': 'db_dictionary_password',
+    'HOST': 'localhost',
+    'PORT': 5432,
+}
 }
 
 
